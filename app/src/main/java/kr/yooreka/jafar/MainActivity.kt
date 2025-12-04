22f2e628ae20b9da16fbdaf5b695aee106c52df5
Jacob <Bracket0723@gmail.com>
2025-12-05 03:46:29 +0900

    ✨: Hilt 사용 구조로 변경

--- app/src/main/java/kr/yooreka/jafar/MainActivity.kt
+++ app/src/main/java/kr/yooreka/jafar/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,9 +15,13 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import kr.yooreka.jafar.feat.profile.ProfileScreen
+import dagger.hilt.android.AndroidEntryPoint
 import kr.yooreka.jafar.ui.theme.JafarTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
---
+++ app/src/main/java/kr/yooreka/jafar/MainActivityViewModel.kt
@@ -0,0 +1,11 @@
+package kr.yooreka.jafar
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+) : ViewModel() {
+
+}
