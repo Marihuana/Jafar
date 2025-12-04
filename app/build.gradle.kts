3e5559b531aa1407704a1317b8ffa01f2db2485f
Jacob <Bracket0723@gmail.com>
2025-12-05 05:20:40 +0900

    🔧 : 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.core)
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.material3.window.sizeclass)
     implementation(libs.androidx.constraintlayout.compose)
     implementation(libs.coil.compose)
