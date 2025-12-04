eaf18c858261debe78ce55c5cff4f2819ee32d74
Jacob <Bracket0723@gmail.com>
2025-12-05 04:21:42 +0900

    🏗️: domain, data 레이어 작성
    - Repository 및 구현체 작성
    - 모듈작성

---
+++ app/src/main/java/kr/yooreka/jafar/data/local/CareerRepositoryImpl.kt
@@ -0,0 +1,7 @@
+package kr.yooreka.jafar.data.local
+
+import kr.yooreka.jafar.domain.repository.CareerRepository
+import javax.inject.Inject
+
+class CareerRepositoryImpl @Inject constructor() : CareerRepository {
+}
---
+++ app/src/main/java/kr/yooreka/jafar/data/local/PortfolioRepositoryImpl.kt
@@ -0,0 +1,7 @@
+package kr.yooreka.jafar.data.local
+
+import kr.yooreka.jafar.domain.repository.PortfolioRepository
+import javax.inject.Inject
+
+class PortfolioRepositoryImpl @Inject constructor() : PortfolioRepository {
+}
